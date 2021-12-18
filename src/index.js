9195abfe2704a0fb9f7723f3c0c24577f8438647
H4KB <akabaharuka@gmail.com>
2021-12-18 16:03:49 +0900

    feat: Add 'O' turn.

--- src/index.js
+++ src/index.js
@@ -15,13 +15,21 @@ function Square(props) {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
+      if (this.state.squares[i]) {
+        return;
+      }
+
       const squares = this.state.squares.slice();
-      squares[i] = 'X'
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
 
     renderSquare(i) {
@@ -34,7 +42,7 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
   
       return (
         <div>
