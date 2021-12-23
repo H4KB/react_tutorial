7e99baaa899993caf1c81dbf687785a184861b8d
H4KB <akabaharuka@gmail.com>
2021-12-23 22:47:12 +0900

    chore: Add eslint and prettier.

--- TypeScript/src/index.tsx
+++ TypeScript/src/index.tsx
@@ -3,66 +3,58 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
-    }
+  render() {
+    return <button className="square">{/* TODO */}</button>;
   }
-  
-class Board extends React.Component {
-renderSquare(i: Number) {
-    return <Square />;
 }
 
-render() {
+class Board extends React.Component {
+  renderSquare(i: Number) {
+    return <Square />;
+  }
+
+  render() {
     const status = 'Next player: X';
 
     return (
-    <div>
+      <div>
         <div className="status">{status}</div>
         <div className="board-row">
-        {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
         <div className="board-row">
-        {this.renderSquare(3)}
-        {this.renderSquare(4)}
-        {this.renderSquare(5)}
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
         <div className="board-row">
-        {this.renderSquare(6)}
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
-    </div>
+      </div>
     );
-}
+  }
 }
 
 class Game extends React.Component {
-render() {
+  render() {
     return (
-    <div className="game">
+      <div className="game">
         <div className="game-board">
-        <Board />
+          <Board />
         </div>
         <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
         </div>
-    </div>
+      </div>
     );
-}
+  }
 }
 
 // ========================================
 
-ReactDOM.render(
-<Game />,
-document.getElementById('root')
-);
-  
+ReactDOM.render(<Game />, document.getElementById('root'));
